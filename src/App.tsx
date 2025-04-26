0d6657568f8ba542f070afc43096d7b8a4ed7fa6
Densedy Isaguirre <admin@densedy.com>
2025-04-27 03:34:24 +0800

    Update date references in DataParser to April 27, 2024 for consistency in filtering video items

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const stackStyles: Partial<IStackStyles> = {
   },
 };
 
-const lastUpdate: Date = new Date("April 20, 2025 12:00 AM +8");
+const lastUpdate: Date = new Date("April 27, 2025 12:00 AM +8");
 const videoItemsViews: IVideoItems[] = [];
 
 initializeIcons();
@@ -325,7 +325,7 @@ export const App: React.FunctionComponent = () => {
   ]
   return (
     <FluentProvider theme={webLightTheme}>
-      {/* <YouTubeAdmin lastUpdate={new Date()}/> */}
+     {/* <YouTubeAdmin lastUpdate={new Date()}/> */}
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         <Image
           // src={"http://localhost:3000/favicon.ico"}
